b3b1381db3ef5a1d7173e946f1b55edf53965b94
Asema <asema.abdurasulova@gmail.com>
2024-11-08 15:36:02 +0600

    created input for to-do

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-const container = document.createElement('div')
-container.className = "container"
+const container = document.createElement("div");
+container.className = "container";
 
-const box = document.createElement('div')
-box.className = 'box'
+const box = document.createElement("div");
+box.className = "box";
 
-container.prepend(box)
+const input = document.createElement("input");
+input.className = "task";
+input.placeholder = "Enter yout task";
+
+container.prepend(box);
